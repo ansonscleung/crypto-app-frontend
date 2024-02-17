293d3aaca244d0cfcdabb8ee34bda9df794e81c5
Anson Leung <ansonscleung@gmail.com>
2024-02-18 02:18:51 +0800

    Initialize Express.js with CoinMarketCap API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+const COINMARKETCAP_API_DOMAIN =
+  process.env.COINMARKETCAP_API_DOMAIN ||
+  "https://sandbox-api.coinmarketcap.com";
+const COINMARKETCAP_API_KEY =
+  process.env.COINMARKETCAP_API_KEY || "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `${COINMARKETCAP_API_DOMAIN}/v2/cryptocurrency/quotes/latest?symbol=BTC&convert=USD`,
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "*",
+          "Access-Control-Allow-Headers":
+            "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+          "Content-Type": "application/json",
+          "X-CMC_PRO_API_KEY": COINMARKETCAP_API_KEY,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data ? (
+        <div>
+          <h1>Data from API:</h1>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
