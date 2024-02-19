4d278d892e19ff449f59efb39a627ad3bf283a87
Anson Leung <ansonscleung@gmail.com>
2024-02-20 00:24:33 +0800

    Demo call from backend; add dotenv for environment setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import React, { useEffect, useState } from "react";
-const COINMARKETCAP_API_DOMAIN =
-  process.env.COINMARKETCAP_API_DOMAIN ||
-  "https://sandbox-api.coinmarketcap.com";
-const COINMARKETCAP_API_KEY =
-  process.env.COINMARKETCAP_API_KEY || "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c";
+const API_DOMAIN = process.env.API_DOMAIN || "http://localhost:5000";
 
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(
-      `${COINMARKETCAP_API_DOMAIN}/v2/cryptocurrency/quotes/latest?symbol=BTC&convert=USD`,
-      {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "*",
-          "Access-Control-Allow-Headers":
-            "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
-          "Content-Type": "application/json",
-          "X-CMC_PRO_API_KEY": COINMARKETCAP_API_KEY,
-        },
-      }
-    )
+    fetch(`${API_DOMAIN}/api/quotes`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error("Error:", error));
@@ -31,8 +19,8 @@ function App() {
     <div>
       {data ? (
         <div>
-          <h1>Data from API:</h1>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
+          <p>Bitcoin</p>
+          <p>${data["BTC"]}</p>
         </div>
       ) : (
         <p>Loading...</p>
