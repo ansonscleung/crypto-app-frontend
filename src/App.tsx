54579dacc18d5a56da2a915a34df02e35cc73e25
Anson Leung <ansonscleung@gmail.com>
2024-02-20 02:11:20 +0800

    Demo API call with BTC/ETH/LTC-USD pairs

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import React, { useEffect, useState } from "react";
 const API_DOMAIN = process.env.API_DOMAIN || "http://localhost:5000";
 
 function App() {
-  const [data, setData] = useState(null);
+  type QuoteProps = {
+    name: String;
+    price: String;
+    volume: Number;
+    change: Number;
+  };
+
+  const [data, setData] = useState<Array<QuoteProps> | null>(null);
 
   useEffect(() => {
     fetch(`${API_DOMAIN}/api/quotes`, {
@@ -15,15 +22,23 @@ function App() {
       .catch((error) => console.error("Error:", error));
   }, []);
 
+  const Quote = ({ name, price, volume, change }: QuoteProps) => {
+    return (
+      <div>
+        <p>{name}</p>
+        <p>${price}</p>
+        <p>Volume: {volume.toString()}</p>
+        <p>Change: {change.toString()}</p>
+      </div>
+    );
+  };
+
   return (
     <div>
       {data ? (
-        <div>
-          <p>Bitcoin</p>
-          <p>${data["BTC"]["price"]}</p>
-          <p>Volume: {data["BTC"]["volume"]}</p>
-          <p>Change: {data["BTC"]["change"]}</p>
-        </div>
+        data.map(({ name, price, volume, change }: QuoteProps) => (
+          <Quote name={name} price={price} volume={volume} change={change} />
+        ))
       ) : (
         <p>Loading...</p>
       )}
